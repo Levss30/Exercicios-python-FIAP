a082c568781ad06d8daa1b6172b08fc2f456955d
Lucas <lucas.levasseur18@gmail.com>
2024-11-04 21:11:53 -0300

    Tuplas - Capitulo 8

---
+++ Cap - 8/tuplas.py
@@ -0,0 +1,10 @@
+categorias = ("padawan", "knight", "master")
+
+# teste1, teste2, teste3 = categorias
+
+# print(teste1)
+# print(teste2)
+# print(teste3)
+
+for categoria in categorias:
+    print(categoria)
